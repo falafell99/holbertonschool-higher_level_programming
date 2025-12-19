71eabd7ea61553dc6624c42044565aa4c236235b
Rafael <12767@holbertonstudents.com>
2025-12-19 14:00:41 +0000

    Add task 7: 7-add_tuple.py - Add two tuples

---
+++ python-data_structures/7-main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+add_tuple = __import__('7-add_tuple').add_tuple
+
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
+
+# Дополнительные тесты
+print(add_tuple((), ()))
+print(add_tuple((1, 2, 3, 4), (5, 6)))
+print(add_tuple((10,), (20, 30)))
