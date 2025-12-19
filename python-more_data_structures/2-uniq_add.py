06c99505d2c4c00cbd595b1de95b56c10ff37474
Rafael <12767@holbertonstudents.com>
2025-12-19 18:54:47 +0000

    Add task 2: 2-uniq_add.py - Add unique integers

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    """Add all unique integers in a list."""
+    unique_nums = set()
+    total = 0
+    for num in my_list:
+        if num not in unique_nums:
+            unique_nums.add(num)
+            total += num
+    return total
