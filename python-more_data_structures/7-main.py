6aa80c260ea2e6736bd194f822902265a85f450e
Rafael <12767@holbertonstudents.com>
2025-12-19 20:46:19 +0000

    Add task 7: 7-update_dictionary.py - Replace or add key/value in dictionary

---
+++ python-more_data_structures/7-main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+update_dictionary = __import__('7-update_dictionary').update_dictionary
+print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
+
+a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+new_dict = update_dictionary(a_dictionary, 'language', "Python")
+print_sorted_dictionary(new_dict)
+print("--")
+print_sorted_dictionary(a_dictionary)
+
+print("--")
+print("--")
+
+new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
+print_sorted_dictionary(new_dict)
+print("--")
+print_sorted_dictionary(a_dictionary)
+
+# Дополнительные тесты
+print("\n--- Additional tests ---")
+empty_dict = {}
+update_dictionary(empty_dict, 'new_key', 'new_value')
+print_sorted_dictionary(empty_dict)
