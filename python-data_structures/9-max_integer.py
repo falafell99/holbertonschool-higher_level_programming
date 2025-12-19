4235e4d91e0ec75b5bea7b665c6a33e809987ee7
Rafael <12767@holbertonstudents.com>
2025-12-19 14:07:02 +0000

    Fix pycodestyle: Remove whitespace from blank line in 9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,6 @@ def max_integer(my_list=[]):
     """Find the biggest integer of a list."""
     if len(my_list) == 0:
         return None
-    
     max_num = my_list[0]
     for num in my_list:
         if num > max_num:
