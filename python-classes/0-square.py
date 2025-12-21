8c0c95d102b34229beb7a578c6717609b13b90df
Rafael <12767@holbertonstudents.com>
2025-12-21 22:12:33 +0000

    Fix PEP8: 0-square.py with correct formatting

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,14 +2,7 @@
 """Square class definition."""
 
 
-
 class Square:
     """Defines a square by its size."""
-
     def __init__(self, size):
-        """Initialize a new Square.
-
-        Args:
-            size: The size of the square.
-        """
         self.__size = size
