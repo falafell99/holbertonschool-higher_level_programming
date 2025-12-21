6438a5819823179cdecba0f77b6bceb4dd3fad36
Rafael <12767@holbertonstudents.com>
2025-12-21 19:52:21 +0000

    Fix PEP8: Correct blank lines formatting in 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Square class definition."""
 
+
 class Square:
     """Defines a square by its size."""
-    
+
     def __init__(self, size):
         """Initialize a new Square.
-        
+
         Args:
             size: The size of the square.
         """
