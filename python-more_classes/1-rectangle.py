dba0b3eaf858c7110b0547451838635e28edde5a
Rafael <12767@holbertonstudents.com>
2025-12-22 11:27:33 +0000

    Fix pycodestyle and add tasks 0-1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -4,29 +4,29 @@
 
 class Rectangle:
     """Defines a rectangle with width and height."""
-    
+
     def __init__(self, width=0, height=0):
         """Initialize rectangle with width and height.
-        
+
         Args:
             width (int): Width of rectangle. Defaults to 0.
             height (int): Height of rectangle. Defaults to 0.
         """
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         """Get width of rectangle."""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """Set width with validation.
-        
+
         Args:
             value (int): New width value.
-            
+
         Raises:
             TypeError: If value is not integer.
             ValueError: If value is less than 0.
@@ -36,19 +36,19 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
         """Get height of rectangle."""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """Set height with validation.
-        
+
         Args:
             value (int): New height value.
-            
+
         Raises:
             TypeError: If value is not integer.
             ValueError: If value is less than 0.
