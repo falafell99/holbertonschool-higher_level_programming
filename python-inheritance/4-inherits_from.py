2e2fcd88a264666b1aad083eabe7031de78f7ead
Rafael <12767@holbertonstudents.com>
2025-12-24 18:03:23 +0000

    Fix PEP8: remove trailing whitespace in 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,11 +4,9 @@
 
 def inherits_from(obj, a_class):
     """Check if object inherits from specified class.
-    
     Args:
         obj: Object to check.
         a_class: Class to compare against.
-        
     Returns:
         bool: True if obj inherits from a_class, False otherwise.
     """
