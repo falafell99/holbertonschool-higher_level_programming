06ab8ad77b88ad0e4576b9b50d0fb988e6955ad0
Rafael <12767@holbertonstudents.com>
2025-12-24 18:08:20 +0000

    Add task 6: 6-base_geometry.py - BaseGeometry with area method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""BaseGeometry class with area method."""
+
+
+class BaseGeometry:
+    """Base geometry class."""
+
+    def area(self):
+        """Calculate area of geometry.
+        Raises:
+            Exception: area() is not implemented
+        """
+        raise Exception("area() is not implemented")
