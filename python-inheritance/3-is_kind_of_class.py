5e1a3c5b54316e28136fd0020008119ba19a9f44
Rafael <12767@holbertonstudents.com>
2025-12-24 18:01:29 +0000

    Fix PEP8: remove trailing whitespace in 3-is_kind_of_class.py

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -4,11 +4,9 @@
 
 def is_kind_of_class(obj, a_class):
     """Check if object is instance of or inherits from specified class.
-    
     Args:
         obj: Object to check.
         a_class: Class to compare against.
-        
     Returns:
         bool: True if obj is instance of a_class or inherited from it.
     """
