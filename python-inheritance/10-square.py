57c0f63a2293625a03ea8b3f8d2492c5f7ff7451
Rafael <12767@holbertonstudents.com>
2025-12-25 12:48:34 +0000

    Add task 10: 10-square.py - Square inheriting from Rectangle

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square class that inherits from Rectangle."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherits from Rectangle."""
+
+    def __init__(self, size):
+        """Initialize square with size.
+        Args:
+            size (int): Size of square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
