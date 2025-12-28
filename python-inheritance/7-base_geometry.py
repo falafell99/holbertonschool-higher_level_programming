cbc0439896a9a524e0de0de03583ca731971614f
Rafael <12767@holbertonstudents.com>
2025-12-28 20:18:12 +0000

    Fix Task 7: correct file name and strict type validation

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-"""
-Module for BaseGeometry class.
-"""
+"""Module for BaseGeometry class."""
 
 
 class BaseGeometry:
     """A class representing base geometry."""
 
     def area(self):
-        """Method to calculate area, not implemented yet."""
+        """Method that raises an Exception because it's not implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """
         Validates value as an integer.
-
+        
         Args:
             name (str): The name of the value.
             value (int): The value to validate.
-
+            
         Raises:
             TypeError: If value is not an integer.
-            ValueError: If value is less or equal to 0.
+            ValueError: If value is <= 0.
         """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
