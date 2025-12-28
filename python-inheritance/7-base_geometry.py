d804aa2c37a627de2c2f179c029bc0c078177b7a
Rafael <12767@holbertonstudents.com>
2025-12-28 20:05:41 +0000

    Fix task 7: strict type check and docstrings

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
-"""BaseGeometry class with integer validator."""
+"""
+Module 7-base_geometry
+Defines a class BaseGeometry
+"""
 
 
 class BaseGeometry:
-    """Base geometry class."""
+    """Class BaseGeometry"""
 
     def area(self):
-        """Calculate area of geometry.
-        Raises:
-            Exception: area() is not implemented
-        """
+        """Method that raises an Exception"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate integer value.
-        Args:
-            name (str): Name of the value.
-            value (int): Value to validate.
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
-        """
-        if type(value) != int:
+        """Method that validates value"""
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
