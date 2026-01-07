5381162514e0a69abb6c508ee9d2a5426781e8fa
Rafael <12767@holbertonstudents.com>
2026-01-07 20:21:29 +0000

    Fix: Remove extra print() calls to use only one print

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("".join(chr(letter) for letter in range(97, 123)), end="")
+for letter in range(97, 123):
+    print(chr(letter), end="")
--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print("".join(chr(letter) for letter in range(97, 123) if letter not in [101, 113]), end="")
+for letter in range(97, 123):
+    if letter != 101 and letter != 113:
+        print(chr(letter), end="")
