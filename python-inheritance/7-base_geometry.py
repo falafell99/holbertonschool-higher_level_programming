5df942cd03985f7dd7220edce217b5bb13d7bf42
Rafael <12767@holbertonstudents.com>
2026-01-07 20:36:04 +0000

    Task 7: Strict type validation and PEP8 fix

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
 """
 Module 7-base_geometry
-Defines a class BaseGeometry
+Defines a class BaseGeometry with public instance methods
 """
 
 
 class BaseGeometry:
-    """Class BaseGeometry"""
+    """A class representing base geometry"""
 
     def area(self):
-        """Method that raises an Exception"""
+        """
+        Public instance method that raises an Exception
+        with the message 'area() is not implemented'
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """
-        Method that validates value:
+        Public instance method that validates value:
         - name is always a string
         - value must be an integer, otherwise raise TypeError
-        - value must be > 0, otherwise raise ValueError
+        - value must be greater than 0, otherwise raise ValueError
         """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-
